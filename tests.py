a87cda5e6ebbf446f52b7d1e839e3c22b215baae
ge36bob <lukas.seitz@tum.de>
2021-11-09 11:22:15 +0100

    New test function

--- tests.py
+++ tests.py
@@ -2,27 +2,27 @@
 # $ python -i filename.py
 
 # RANGE and lists
-numbers = list(range(1,10))
+numbers = list(range(1, 10))
 contains_3 = 3 in numbers
 
 # COPY LISTS by REFERENCE AND VALUE
 copy_reference = numbers
 copy_value = numbers[:]
-copy_value = numbers.copy()
+copy_numbers = numbers.copy()
 
 # COMPREHENSIONS
 squares = []
 for x in range(11):
-    squares.append(x**2)
+    squares.append(x ** 2)
 
-squares_com = [x**2 for x in range(11)]
+squares_com = [x ** 2 for x in range(11)]
 
 # DICTIONARIES
 alien = {
-    'color' : 'grenn',
-    'age' : '2022',
-    'points' : 5,
-    'coords' : [234,534,345]
+    'color': 'grenn',
+    'age': '2022',
+    'points': 5,
+    'coords': [234, 534, 345]
 }
 
 # How to loop over a dictionary
@@ -31,6 +31,11 @@ for key, value in alien.items():
 length = len(alien)
 
 # ARRAYS => LISTS in Python
-a1 = [1,2,3,4,5]
-a2 = [[1,2,3],[4,5,6],88]
+a1 = [1, 2, 3, 4, 5]
+a2 = [[1, 2, 3], [4, 5, 6], 88]
 a2_value = a2[1][2]
+
+
+# Function
+def my_func(a, b):
+    return a ** 2 + b
